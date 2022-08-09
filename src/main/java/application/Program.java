040b13fcdc20a0bb5546b978d6e8f806b945f4f0
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2022-08-09 11:59:53 -0300

    Update Program.java

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 
 
+import model.dao.DaoFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,5 +17,7 @@ public class Program {
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
         System.out.println(seller);
+
+        SellerDAO sellerDAO = DaoFactory.createSellerDao();
     }
 }
