70fbe3c8dbf7cdc7fc01f8fe088f935b181d6c0e
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2022-08-09 11:22:52 -0300

    Update db connections

--- src/main/java/db/DB.java
+++ src/main/java/db/DB.java
@@ -38,7 +38,7 @@ public class DB {
     }
 
     private static Properties loadProperties() {
-        try (FileInputStream fs = new FileInputStream("db.properties")) {
+        try (FileInputStream fs = new FileInputStream("src/db.properties")) {
             Properties props = new Properties();
             props.load(fs);
             return props;
