578a17162489b7f3d25a64339f254a3eb2d7671e
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2022-08-09 13:35:35 -0300

    findById implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -15,9 +15,8 @@ public class Program {
         Department obj = new Department(1, "Books");
         System.out.println(obj);
 
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-        System.out.println(seller);
-
         SellerDAO sellerDAO = DaoFactory.createSellerDao();
+        Seller seller = sellerDAO.findById(3);
+        System.out.println(seller);
     }
 }
